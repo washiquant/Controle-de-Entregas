4acfac6e95e839827bc84e070d627f32aec8a90c
WashiQuant <primarios108@gmail.com>
2026-07-04 10:27:24 -0300

    Lançado arquivo de melhorias e teste de novas funções.

---
+++ tests/tests.py
@@ -0,0 +1,20 @@
+#TRATATIVA DE ENTRADAS
+def validar_numero_comanda(numero_comanda:str):
+    if not numero_comanda.strip():
+        raise ValueError("Numero da comanda obrigatório.")
+
+    if not numero_comanda.isdigit():
+        raise ValueError("A comanda deve conter apenas numeros reais")
+
+
+def validar_valor_entrega(valor_entrega):
+    if not valor_entrega.strip():
+        raise ValueError("Numero da comanda obrigatório.")
+
+    if not valor_entrega.isdigit():
+        raise ValueError("A comanda deve conter apenas numeros reais")
+
+
+
+comanda = input("Qual o numero da comanda ?")
+validar_numero_comanda(comanda)
